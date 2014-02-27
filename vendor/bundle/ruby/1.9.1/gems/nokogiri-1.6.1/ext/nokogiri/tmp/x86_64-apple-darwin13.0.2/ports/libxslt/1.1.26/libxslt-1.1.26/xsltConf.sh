cf0195f47a15affa27756c86ec9bb35632982286
dai <snaqueen@gmail.com>
2014-02-27 22:23:23 +0900

    Tutorial: ~3.2

---
+++ vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.1/ext/nokogiri/tmp/x86_64-apple-darwin13.0.2/ports/libxslt/1.1.26/libxslt-1.1.26/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/dai/rails_app/sample_app2/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/Users/dai/rails_app/sample_app2/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
+XSLT_INCLUDEDIR="-I/Users/dai/rails_app/sample_app2/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
