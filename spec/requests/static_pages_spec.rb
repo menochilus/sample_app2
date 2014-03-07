f5b7732cc301fdf37b6f6a1857ab380d05b45fe6
dai <snaqueen@gmail.com>
2014-03-07 15:17:15 +0900

    finish to 5.3.0

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,52 +7,52 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => "Sample App")
+      page.should have_selector('h1', text: "Sample App")
     end
     it "should have the base title 'Ruby on Rails Tutorial Sample App" do
       visit '/static_pages/home'
       page.should have_selector('title',
-				      	:text => "#{base_title}")
+				      	text: "#{base_title}")
     end
      it "should have a custum page title '| Home'" do
       visit '/static_pages/home'
-      page.should_not have_selector('title', :text => "| Home")
+      page.should_not have_selector('title', text: "| Home")
     end
 end
 
   describe "Help page" do
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => "Help")
+      page.should have_selector('h1', text: "Help")
     end
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-				      	:text => "#{base_title} | Help")
+				      	text: "#{base_title} | Help")
     end
   end
 
   describe "About page" do
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('h1', :text => "About Us")
+      page.should have_selector('h1', text: "About Us")
     end
     it "should have the right title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-				      	:text => "#{base_title} | About Us")
+				      	text: "#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
     it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('h1', :text => "Contact")
+      page.should have_selector('h1', text: "Contact")
     end
     it "should have the right title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                :text => "#{base_title} | Contact")
+                title: "#{base_title} | Contact")
     end
   end
 
