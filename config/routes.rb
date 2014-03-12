d7308bc83d21583f8c95e791b7d7125bc2b8710a
dai <snaqueen@gmail.com>
2014-03-12 22:05:03 +0900

    finish to 7.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp2::Application.routes.draw do
-  get "users/new"
+  #get "users/new"
+  resources :users
 
   #get "static_pages/home"
   #get "static_pages/help"
@@ -11,6 +12,7 @@ SampleApp2::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
