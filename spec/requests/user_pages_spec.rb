0922f5828d8d01cfcb42189044db063eba14f63e
dai <snaqueen@gmail.com>
2014-03-15 23:50:05 +0900

    finish to 7.3.1

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "User Pages" do
 
 	subject { page }
 
-	describe "User pages" do
+	describe "signup page" do
 		before { visit signup_path }
 
 		it { should have_selector('h1',    text: 'Sign up') }
