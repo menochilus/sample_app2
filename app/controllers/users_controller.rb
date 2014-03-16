cb528d75224cdef18e4169e740e5677dd99891cc
dai <snaqueen@gmail.com>
2014-03-16 10:43:14 +0900

    finish to 7.4.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
   		#ユーザー表示ページに移動
   		redirect_to @user
   	else
