cec22617cc71d31cad6070a59b25615d7827def5
dai <snaqueen@gmail.com>
2014-03-16 10:21:48 +0900

    finish to 7.4.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,10 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-  		#Handle a successful save.
+  		#ユーザー表示ページに移動
+  		redirect_to @user
   	else
+  		#入力エラーのときはnew.html.erbに飛ばす
   		render 'new'
   	end
   end
