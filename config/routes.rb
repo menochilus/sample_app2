756c97d953cb5ae1732e521bae1e4e620107c9bd
dai <snaqueen@gmail.com>
2014-03-18 22:15:58 +0900

    finish to 8.1.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp2::Application.routes.draw do
   #get "users/new"
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   #get "static_pages/home"
   #get "static_pages/help"
@@ -12,7 +13,8 @@ SampleApp2::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
